b29ff3f14b388b4c5576ffcc5a9bbb61ef0154f6
MoabF <moabfreddossanto@gmail.com>
2024-10-07 12:54:58 -0300

    questao2

---
+++ Lista_1/questao_2.java
@@ -0,0 +1,11 @@
+public class questao_2{
+    public static void main(String[] args){
+        int L = 8;
+       // Scanner scanner = new Scanner(system.in);
+        int area = 0; int perimetro = 0;
+        area = L * L; 
+        perimetro = L * 4;
+        System.out.println("A área é: " + area + " cm");
+        System.out.println("O perímetro é: " + perimetro + " cm");
+    }
+}
