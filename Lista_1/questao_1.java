0b0ff2e4bb42583fe303f44e22183a897d5222b1
MoabF <moabfreddossanto@gmail.com>
2024-10-07 10:47:06 -0300

    questao1

--- Lista_1/questao_1.java
+++ Lista_1/questao_1.java
@@ -1,5 +1,11 @@
-public class questao1{
+public class questao_1{
     public static void main(String[] args){
-        
+        float B = 7.5f; float H = 12.3f;
+       // Scanner scanner = new Scanner(system.in);
+        float area = 0.0f; float perimetro = 0.0f;
+        area = B * H; 
+        perimetro = (B * 2) + (H * 2);
+        System.out.println("A área é: " + area);
+        System.out.println("O perímetro é: " + perimetro);
     }
 }
