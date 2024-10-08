39fb9c64cc50ebd995970dafa44d770f70e4291d
Moab Varela <109436938+MoabF@users.noreply.github.com>
2024-10-08 20:07:29 -0300

    Add files via upload

---
+++ exemplos_dos_slides/Main (3).java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    int num = 0;
+		System.out.println("Digite o seu número da sorte (entre 0 e 10):");
+		num = scanner.nextInt(); 
+	    int valor = (int)(Math.random() * 11);
+	    
+	    if(valor == num){
+	        System.out.println("Parabéns, você ganhou!");
+	    }
+	    else{
+	         System.out.println("Que pena, o número sorteado foi: " + valor);
+	    }
+	   
+	}
+}
