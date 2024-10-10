1418bea162cfa2352ac180965b68e9735e09064a
Moab Varela <109436938+MoabF@users.noreply.github.com>
2024-10-10 19:20:45 -0300

    Add files via upload

---
+++ exemplos_dos_slides/Main (4).java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    int n = 10;
+	    int v[] = new int[n];
+		System.out.println("Digite os números que deseja ordenar:");
+		for(int i = 0; i<n; i++){
+		    v[i] = scanner.nextInt();
+		}
+		for(int i = n; i>0; i--){
+		    for(int j = 0; j<i-1; j++){
+		    if(v[j] > v[j+1]){
+    		    int aux = v[j];
+    		     v[j] = v[j+1];
+    		     v[j+1] = aux;
+		}
+		    }
+		}
+		System.out.println("A sequência de números ordenada é:");
+		for(int i = 0; i<n; i++){
+		   	System.out.print(v[i] + " ");
+		}
+	}
+}
