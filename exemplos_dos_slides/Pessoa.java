8847fa68ecd788039d633c8d1fa6bd387b90fcb8
Moab Varela <109436938+MoabF@users.noreply.github.com>
2024-10-15 19:19:25 -0300

    Add files via upload

---
+++ exemplos_dos_slides/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa
+{
+    public String nome = "Gabriel";
+    public int idade = 28;
+    
+	public static void main(String[] args) {
+	    Pessoa gabriel = new Pessoa();
+		System.out.println("A idade de " + gabriel.nome + " eh "+
+		                  gabriel.idade + " anos");
+	}
+}
