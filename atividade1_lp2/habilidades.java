8da0bdd5a0c0b537ad4f6a75c716abb0cb4a54ef
Moab Varela <109436938+MoabF@users.noreply.github.com>
2024-10-15 20:11:00 -0300

    Create habilidades.java

---
+++ atividade1_lp2/habilidades.java
@@ -0,0 +1,28 @@
+public classs habilidades{
+	boolean flame = false;
+	boolean yoshi = false;
+  boolean mushroom = false;
+	public states_habilidades(fogo, yosh, cogumelo){
+	if(fogo == true){
+		flame = true;
+  }
+  else{
+  flame = false;  
+  }
+  if(yosh == true){
+		yoshi = true;
+  }
+  else{
+  yoshi = false;
+  }
+
+  if(cogumelo == true){
+		mushroom = true;
+  }
+  else{
+    mushroom  = false;
+  }
+
+  }	
+}
+
