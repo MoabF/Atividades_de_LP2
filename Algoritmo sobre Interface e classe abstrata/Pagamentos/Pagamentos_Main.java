eb852d40c90bf3053ad67a48e50ac9dae2e0b723
Moab Varela <109436938+MoabF@users.noreply.github.com>
2024-11-13 16:04:13 -0300

    Update and rename Pagamentos.java to Pagamentos_Main.java

--- Algoritmo sobre Interface e classe abstrata/Pagamentos/Pagamentos_Main.java
+++ Algoritmo sobre Interface e classe abstrata/Pagamentos/Pagamentos_Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Pagamentos_Main {
     public static void main(String[] args) {
         int n = 0;
         Scanner entrada = new Scanner(System.in);
